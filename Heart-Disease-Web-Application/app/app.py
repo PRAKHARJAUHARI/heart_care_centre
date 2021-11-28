c1379be776fe599ecf26618bd55387fdda536011
PRAKHAR JAUHARI <83902207+PRAKHARJAUHARI@users.noreply.github.com>
2021-11-29 00:00:24 +0530

    Update app.py

--- Heart-Disease-Web-Application/app/app.py
+++ Heart-Disease-Web-Application/app/app.py
@@ -84,7 +84,7 @@ def change_dtypes(df):
 def main():
     """Main function"""
     st.title("❤️ Heart Disease Application ❤️")
-    df = open("heart.csv")
+    df = open(input("heart.csv"))
     st.sidebar.title("Menu")
 
     # Creating radio buttons to choose menu options:
